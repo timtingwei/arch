b74462ae097646f7a213a014727762987d5e1c0a
timtingwei <timtingwei@hotmail.com>
2018-09-19 20:59:25 +0800

    Update picArrange

--- town-design/picArrange.py
+++ town-design/picArrange.py
@@ -12,20 +12,29 @@ import os
 import shutil
 
 # 提取该物件的图层名字(放在数组中)
-def getObjectsLayerNames():
-    return
+def getObjectLayerName():
+	id = rs.GetObject("Please select a object")
+	if id: return rs.ObjectLayer(id)
 
 # 图层名字转换成路径
-def layerNamesToPath():
-    return
+def layerNameToPath(rootPath, layerName):
+	s_lst = layerName.split('::')
+	finalPath = rootPath
+	print(finalPath)
+	for i in range(1, len(s_lst)):
+		finalPath = os.path.join(finalPath, s_lst[i])
+	return finalPath
 
 # 通过路径打开文件
 def viewPic():
-    os.system('open /Users/htwt/timspace/arch/town-design/CY/M-08/01/01.png /Users/htwt/timspace/arch/town-design/CY/M-08/01/02.png')
-    return
+	os.system('open /Users/htwt/timspace/arch/town-design/CY/M-08/01/01.png /Users/htwt/timspace/arch/town-design/CY/M-08/01/02.png')
+	return
 
 
 if (flag):
-    layerNames = getObjectsLayerNames()
-    finalPath = layerNamesToPath(layerNames)
-    viewPic(finalPath)
+	layerNames = getObjectLayerName()
+	a = layerNames
+	rootPath = os.path.abspath('.')
+	finalPath = layerNameToPath(rootPath, layerNames)
+	a = finalPath
+	#viewPic(finalPath)
