abdd4d8fc3eb5e9e90a0fa2936c0979f4793b009
timtingwei <timtingwei@hotmail.com>
2018-09-19 22:54:58 +0800

    实现拾取点跳出图片

--- town-design/picArrange.py
+++ town-design/picArrange.py
@@ -1,7 +1,7 @@
 """Provides a scripting component.
     Inputs:
-        x: The x script variable
-        y: The y script variable
+        flag: The x script variable
+        rootpath: The y script variable
     Output:
         a: The a output variable"""
 # -*- coding: UTF-8 -*-
@@ -19,22 +19,34 @@ def getObjectLayerName():
 # 图层名字转换成路径
 def layerNameToPath(rootPath, layerName):
 	s_lst = layerName.split('::')
-	finalPath = rootPath
+	finalPath =rootPath
 	print(finalPath)
 	for i in range(1, len(s_lst)):
 		finalPath = os.path.join(finalPath, s_lst[i])
 	return finalPath
 
-# 通过路径打开文件
-def viewPic():
-	os.system('open /Users/htwt/timspace/arch/town-design/CY/M-08/01/01.png /Users/htwt/timspace/arch/town-design/CY/M-08/01/02.png')
-	return
+# 通过路径打开多个文件
+def viewPic(prevPath):
+	# 得到文件夹下照片数量cnt
+	L = []
+	for root, dirs, files in os.walk(prevPath):
+		for file in files:
+			if os.path.splitext(file)[1] == '.png':					L.append(os.path.join(root, file))
+	# join中间添加空格
+	open_sh = 'open'
+	for fpath in L:
+		print('fpath = ' + fpath)
+		#appendPath = os.path.join(prevPath, cnt+1)
+		open_sh += ' ' + fpath
+		#os.system('open /Users/htwt/timspace/arch/town-design/CY/M-08/01/01.png /Users/htwt/timspace/arch/town-design/CY/M-08/01/02.png')
+	print(open_sh)
+	os.system(open_sh)
 
 
 if (flag):
 	layerNames = getObjectLayerName()
 	a = layerNames
-	rootPath = os.path.abspath('.')
+	print(a)
 	finalPath = layerNameToPath(rootPath, layerNames)
 	a = finalPath
-	#viewPic(finalPath)
+	viewPic(finalPath)
