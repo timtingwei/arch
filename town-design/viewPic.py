f283b0019252c4869ef524a81d19bd5298390e5d
timtingwei <timtingwei@hotmail.com>
2018-09-19 19:55:00 +0800

    Create: 测试案例

--- town-design/viewPic.py
+++ town-design/viewPic.py
@@ -1,3 +1,4 @@
+
 """
 # -*- coding: UTF-8 -*-
 from PIL import Image
@@ -12,13 +13,49 @@ img2.show()
 """
 
 import os
+from os import listdir
+from os.path import isfile, join
 
-apath = os.path.abspath('.')
+from PIL import Image
+
+"""
+absPath = os.path.abspath('.')
 syspath = os.name
 
-cypath = os.path.join(apath, 'CY')
+# ::CY::M-08::01
+# ~/CY/M-08/01/..
+path_arr = ['CY', 'M-08', '01']
+appendPath = path_arr[0] + '/' + path_arr[1] + '/' + path_arr[2];
+last_path = os.path.join(absPath, appendPath)
+final_path = os.path.join(last_path, '01.png')
 
-print(apath)
+#f = open(final_path, "wb")
+#f.read()
+# f.close()
+
+final_dir = '/Users/htwt/timspace/arch/town-design/CY/M-08/01/01.png'
+img1 = Image.open(final_dir)
+img2 = Image.open('/Users/htwt/timspace/arch/town-design/CY/M-08/01/02.png')
+img1.show()
+img2.show()
+
+
+# store the all files to list
+# onlyfiles = [f for f in listdir(last_path) if isfile(join(last_path, f))]
+
+# open(onlyfiles[0])
+
+    
+
+print(absPath)
 print(syspath)
 
-print(cypath)
+print(final_path)
+"""
+
+def main():
+    os.system('open /Users/htwt/timspace/arch/town-design/CY/M-08/01/01.png')
+    os.system('open /Users/htwt/timspace/arch/town-design/CY/M-08/01/02.png')
+
+# open -a Preview /Users/htwt/timspace/arch/town-design/CY/M-08/01/01.png /Users/htwt/timspace/arch/town-design/CY/M-08/01/02.png
+
