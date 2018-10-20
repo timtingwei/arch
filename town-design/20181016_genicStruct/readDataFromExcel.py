fcb95fd29aee142b4a4d842699e48c4d7722531d
timtingwei <timtingwei@hotmail.com>
2018-10-20 19:25:00 +0800

    Update: import data by python2.7

--- town-design/20181016_genicStruct/readDataFromExcel.py
+++ town-design/20181016_genicStruct/readDataFromExcel.py
@@ -1,4 +1,6 @@
 #-*- coding: utf-8 -*-
+
+"""
 from openpyxl import load_workbook
 
 def getColumnValueList(booksheet, column, start_row):
@@ -48,3 +50,61 @@ if __name__ == '__main__':
         genicDataLst_lst.append(lst)
     print(genicDataLst_lst)
     
+"""
+
+'Python 2.7'
+import sys
+from xlrd import open_workbook
+
+def getColumnValueList(booksheet, column, start_row):
+    # 读取一个booksheet的某一列的值, 可以标记头行
+    lst = []
+    row = start_row
+    value = ''
+    nrows = booksheet.nrows
+    for row in range(start_row, nrows):
+        value = booksheet.cell(row-1, column-1).value
+        if (value == ''):  # 判断空
+            break
+        if isinstance(value, unicode):
+            value = value.encode('utf-8')
+        lst.append(value)
+    return lst
+
+def main():
+    wb = open_workbook('/Users/htwt/Desktop/20181019_totalGenics.xls')
+    tot_sheets = wb.sheets()
+    # 表头
+    #sheet_head = sheets[0]
+    booksheet = tot_sheets[0]
+    tot_arch_type = getColumnValueList(booksheet, 5, 2)   # 所有建筑类型
+    tot_job_type = getColumnValueList(booksheet, 6, 2)    # 所有职业类型
+    tot_trans_type = getColumnValueList(booksheet, 7, 2)  # 所有出行方式类型
+    tot_trans_speed  = getColumnValueList(booksheet, 8, 2)  # 所有出行方式速度
+
+    genicDataLst_lst = []   # 存放所有职业的数据list的list
+    for booksheet in tot_sheets[1:]:
+        lst = []
+        jobName = getColumnValueList(booksheet, 1, 3)    # 该职业名字
+        lst.append(jobName)
+        # 每一个分类结构放在一个list下
+        for i in range(15)[2:15:4]:  # [2, 6, 10, 14]
+            activity = getColumnValueList(booksheet, i, 3)
+            domain = getColumnValueList(booksheet, i+1, 3)
+            place = getColumnValueList(booksheet, i+2, 3)
+            isDup = getColumnValueList(booksheet, i+3, 3)
+            node_lst = [activity, domain, place, isDup]
+            lst.append(node_lst)
+        breakup = getColumnValueList(booksheet, 18, 3)
+        trans_type = getColumnValueList(booksheet, 19, 3)
+        lst.append(breakup)
+        lst.append(trans_type)
+        
+        genicDataLst_lst.append(lst)
+    print(genicDataLst_lst)
+    #print(genicDataLst_lst[0])
+    #print(len(genicDataLst_lst[0]))
+
+
+if __name__ == '__main__':
+    main()
