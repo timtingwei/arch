be5ff9328fa8384ea1b2b6b3ce96fed3977bfc09
timtingwei <timtingwei@hotmail.com>
2018-10-20 17:20:51 +0800

    Create readDataFromExcel.py: read excel in python3

---
+++ town-design/20181016_genicStruct/readDataFromExcel.py
@@ -0,0 +1,39 @@
+#-*- coding: utf-8 -*-
+from openpyxl import load_workbook
+
+def getColumnValueList(booksheet, column, start_row):
+    # 读取一个booksheet的某一列的值, 可以标记头行
+    lst = []
+    row = start_row
+    while (1):
+        value = booksheet.cell(row, column).value
+        if (value == None):
+            break
+        lst.append(value)
+        row = row + 1
+    return lst
+
+
+
+if __name__ == '__main__':
+    wb = load_workbook('/Users/htwt/Desktop/20181019_totalGenics.xlsx')
+    sheets = wb.sheetnames
+    # 表头
+    sheet_head = sheets[0]
+    booksheet = wb[sheet_head]
+    tot_arch_type = getColumnValueList(booksheet, 5, 2)   # 所有建筑类型
+    tot_job_type = getColumnValueList(booksheet, 6, 2)    # 所有职业类型
+    tot_trans_type = getColumnValueList(booksheet, 7, 2)  # 所有出行方式类型
+    tot_trans_speed  = getColumnValueList(booksheet, 8, 2)  # 所有出行方式速度
+
+    genicDataLst_lst = []   # 存放所有职业的数据list的list
+    for sheetName in wb.sheetnames[1:]:
+        lst = []
+        booksheet = wb[sheetName]
+        jobName = getColumnValueList(booksheet, 1, 3)    # 该职业名字
+        lst.append(jobName)
+
+        
+        genicDataLst_lst.append(lst)
+    print(genicDataLst_lst)
+    
