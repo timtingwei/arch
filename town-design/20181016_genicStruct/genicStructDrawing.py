e839cc294bbfc39d4819dd5911c296864f01968f
timtingwei <timtingwei@hotmail.com>
2018-10-22 22:25:26 +0800

    Update: 补齐动画框架中的各个接口数据

--- town-design/20181016_genicStruct/genicStructDrawing.py
+++ town-design/20181016_genicStruct/genicStructDrawing.py
@@ -1,10 +1,4 @@
-﻿"""Provides a scripting component.
-    Inputs:
-        x: The x script variable
-        y: The y script variable
-    Output:
-        a: The a output variable"""
-
+﻿
 __author__ = "htwt"
 __version__ = "2018.10.22"
 
@@ -22,13 +16,13 @@ def getDicts():
     place_dict = createHash(place_index_key, place_pt)
     return road_dict, place_dict
 
-
 def getPlacePtLst(place_lst, place_dict):
     return [place_dict[str(place[0]) + ',' + str(place[1])] for place in place_lst]
+
 def getRoadCurve(trans_path, road_dict):
     # 根据路径结点序号得到一条路径线段
     curve_lst = []
-    trans_path = ([9, 0, 70, 79, 91, 77, 8, 60, 56, 58, 47, 43, 29, 61, 50, 54, 78, 81, 80], 3660.0) 
+    #trans_path = ([9, 0, 70, 79, 91, 77, 8, 60, 56, 58, 47, 43, 29, 61, 50, 54, 78, 81, 80], 3660.0) 
     n = len(trans_path[0])
     for i in range(n-1):
         node, next = trans_path[0][i], trans_path[0][i+1]
@@ -42,61 +36,31 @@ def getRoadCurve(trans_path, road_dict):
     #pt = rs.CurveArcLengthPoint(join_curve, t*length)
     return join_curve
 
-def main():
-    dicts_lst = getDicts()
-    road_dict, place_dict = dicts_lst[0], dicts_lst[1]
-    
-    child_main_lst = child_main()
-    breakup = child_main_lst[0]
-    seq_time = child_main_lst[1]
-    trans_time = child_main_lst[2]
-    place = child_main_lst[3]
+def getChildPtLst(child, place_dict, road_dict, clock_time_range):
+    pt_lst = []       # 一个人的每一帧的点
+    if not child.isArrangeValid:   # 如果该人的事件排列无效的话, 返回空列表
+        return pt_lst
+    # self.trans_path = [([9, 0, 70, 79, 91, 77, 8, 60, 56, 58, 47, 43, 29, 61, 50, 54, 78, 81, 80], 3660.0), ([], d), ([], d)]
 
-    
-    #print('breakup:')
-    #print(breakup)
-    #print('seq_time:')
-    #print(seq_time)
-    #print('trans_time:')
-    #print(trans_time)
-    print('place:')
-    print(place)
-    trans_path = ([9, 0, 70, 79, 91, 77, 8, 60, 56, 58, 47, 43, 29, 61, 50, 54, 78, 81, 80], 3660.0)
-    
+    # child.isArrangeValid, child.breakup, child.time, child.trans_time, child.trans_speed, child.place, child.trans_path
+    # 根据child.place得到区块点表(place_pt_lst), 根据child.trans_path得到道路多段线(road_curve_lst)
     # place_pt_lst: 该对象静态事件发生的点序列
     # road_curve:   该对象交通事件对应的路径线段
-    place_pt_lst = getPlacePtLst(place, place_dict)
-    road_curve = getRoadCurve(trans_path, road_dict)
-
-    """
-    # 设置帧数
-    time_step = 0.1   # 0.1h/帧 = 6min/帧
-    #time_step = 1     # 1h/帧
-    #time_step = 1/60   # 1min/zhen
-    tot_time = 24     # 记录一天的变化
+    place_pt_lst = getPlacePtLst(child.place, place_dict)
+    road_curve_lst = [getRoadCurve(path, road_dict) for path in child.trans_path]
     
-    # 构造打点时间序列
-    temp_sum = 0
-    clock_time_range = []
-    while temp_sum < tot_time:
-        clock_time_range.append(temp_sum)
-        temp_sum = temp_sum + time_step
-    
-    # child.isArrangeValid, child.breakup, child.time, child.trans_time, child.trans_speed, child.place, child.trans_path
-    # 根据child.place得到区块点表(pace_pt_lst), 根据child.trans_path得到道路多段线(road_curve_lst)
     # evaluateCurveByLength() 组合line成polyline, 根据距离得到线上对应点
     # 获得确定起床点, 事件和交通情况下, 时间结点序列
-    seqTime = [0, breakup]
-    temp_sum = breakup
-    for i in range(len(seq_time)-1):
-        temp_sum = temp_sum + seq_time[i]
+    seqTime = [0, child.breakup]
+    temp_sum = child.breakup
+    for i in range(len(child.time)-1):
+        temp_sum = temp_sum + child.time[i]
         seqTime.append(temp_sum)
         temp_sum = temp_sum + trans_time[i]
         seqTime.append(temp_sum)
-    seqTime.append(temp_sum + seq_time[-1])
-    # print(seqTime[-1]-breakup)   # right
+    seqTime.append(temp_sum + child.time[-1])
+    # print(seqTime[-1]-child.breakup)   # right
 
-    pt_lst = []       # 一个人的每一帧的点
     seq_p = 0         # 在序列上标记时间的指针
     for t in clock_time_range:
         while seqTime[seq_p+1] <= t:   # 用指针找到对应区间
@@ -115,8 +79,30 @@ def main():
                 length = trans_path[int((seq_p-1)/2)][1]*scale_t
                 pt = rs.CurveArcLengthPoint(road_curve, length)
         pt_lst.append(pt)
-    """
-    return road_curve
+    return pt_lst
+
+def main():
+    dicts_lst = getDicts()
+    road_dict, place_dict = dicts_lst[0], dicts_lst[1]
+
+    # 设置帧数
+    time_step = 0.1   # 0.1h/帧 = 6min/帧
+    #time_step = 1     # 1h/帧
+    #time_step = 1/60   # 1min/zhen
+    tot_time = 24     # 记录一天的变化
+    
+    # 构造打点时间序列
+    temp_sum = 0
+    clock_time_range = []
+    while temp_sum < tot_time:
+        clock_time_range.append(temp_sum)
+        temp_sum = temp_sum + time_step
+
+    pts_lst = []      # 所有人的帧数点的集合
+    child_lst = []    # 从基因条py中传入所有的子对象
+    for child in child_lst:
+        pts_lst.append(getChildPtLst(child, place_dict, road_dict, clock_time_range))
+    return pts_lst
     
 
 a = main()
