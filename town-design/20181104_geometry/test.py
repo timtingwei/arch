867e0eae93000aef44e48b77b931714b36cc5049
timtingwei <timtingwei@hotmail.com>
2018-11-07 00:13:58 +0800

    Update: test class instance

--- town-design/20181104_geometry/test.py
+++ town-design/20181104_geometry/test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+"""
 class Parent(object):
     def __init__(self, a):
         self.a = a
@@ -20,6 +21,20 @@ def main():
     print(p.a)
     child = p.initChildInParent(3)
     print(child.b)
+
+"""
+
+class Parent(object):
+    def __init__(self, a):
+        self.value = a
+        return
+
+def main():
+    parent = Parent(4)
+    jack = Parent(parent.value)
+    jack.value = 12
+    print(parent.value)
+    print(jack.value)
 
 if __name__ == '__main__':
     main()
