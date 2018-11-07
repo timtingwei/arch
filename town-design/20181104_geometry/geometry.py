4b8c71b2fd24598e3905fc6b886b4c479eac3cde
timtingwei <timtingwei@hotmail.com>
2018-11-07 21:05:39 +0800

    Update: 补写两个向量平行Vector::isVectorParallel()方法

--- town-design/20181104_geometry/geometry.py
+++ town-design/20181104_geometry/geometry.py
@@ -252,8 +252,9 @@ class Vector(object):
         return math.sqrt(self.x*self.x+self.y*self.y)
 
     def isVectorParallel(self, vec):
-        dot()
         # 判断实例向量和vec之间是否平行
+	if self.cross(self, vec) == 0: return True
+	else: return False
 
 class Polyline(object):
     """
@@ -422,7 +423,8 @@ class RectangleRelation(object):
     def isParallel(self):
         # 根据向量计算两个矩形是否平行
         parallel = False
-        self.rec1.vec_lst[0].isVectorParallel(self.rec2.vec_lst[0])
+        if self.rec1.vec_lst[0].isVectorParallel(self.rec2.vec_lst[0]) == True:
+	    parallel = True
         return parallel
 
     def getGapClass(self):
